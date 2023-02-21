50dde7ad907a33d6353878e86ff1e8b621788c79
shaa <rtp3386@gmail.com>
2023-02-21 22:23:25 +0900

    [MVC 프론트 컨트롤러 패턴 도입, View 분리] 완료

---
+++ servlet/src/main/java/hello/servlet/domain/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,16 @@
+package hello.servlet.domain.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.web.frontcontroller.v2.MyView;
+import hello.servlet.domain.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
