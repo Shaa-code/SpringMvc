165ffe9de31cbddf6ea5fef54f2c377d29cabde6
shaa <rtp3386@gmail.com>
2023-03-11 23:54:19 +0900

    어댑터 패턴 인터페이스 추가 support()는 핸들러(컨트롤러)를 받아서 사용할 수 있는지 없는지 여부를 체크한다. handle()은 실제 컨트롤러를 호출하고 ModelView를 반환하는 역할을 한다.

---
+++ servlet/src/main/java/hello/servlet/domain/web/frontcontroller/MyHandlerAdapter.java
@@ -0,0 +1,11 @@
+package hello.servlet.domain.web.frontcontroller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+    boolean support(Object handler);
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+}
