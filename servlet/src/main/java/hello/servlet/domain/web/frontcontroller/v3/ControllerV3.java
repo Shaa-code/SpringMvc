293228dbbb2260d04808b82f942e31638f88954a
shaa <rtp3386@gmail.com>
2023-03-26 23:52:05 +0900

    "스프링 MVC - 시작하기" 까지 끝내기

--- servlet/src/main/java/hello/servlet/domain/web/frontcontroller/v3/ControllerV3.java
+++ servlet/src/main/java/hello/servlet/domain/web/frontcontroller/v3/ControllerV3.java
@@ -5,6 +5,5 @@ import hello.servlet.domain.web.frontcontroller.ModelView;
 import java.util.Map;
 
 public interface ControllerV3 {
-
     ModelView process(Map<String, String> paramMap);
 }
--- servlet/src/main/java/hello/servlet/domain/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ servlet/src/main/java/hello/servlet/domain/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -3,8 +3,8 @@ package hello.servlet.domain.web.frontcontroller.v5.adapter;
 import hello.servlet.domain.web.frontcontroller.ModelView;
 import hello.servlet.domain.web.frontcontroller.MyHandlerAdapter;
 import hello.servlet.domain.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.domain.web.frontcontroller.v4.ControllerV4;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -12,17 +12,23 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
     @Override
     public boolean support(Object handler) {
-        return (handler instanceof ControllerV3);
+        return (handler instanceof ControllerV4);
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
-        ControllerV3 controller = (ControllerV3) handler;
+        ControllerV4 controller = (ControllerV4) handler;
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+
         return mv;
     }
 
