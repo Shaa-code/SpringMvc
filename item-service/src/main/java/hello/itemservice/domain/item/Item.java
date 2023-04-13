4dca744a5d75644b491ea96ac3fb5e9afda4f4e0
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-04-14 00:46:52 +0900

    스프링 MVC - 웹 페이지 만들기 (상품 목록 - 타임리프) 끝

--- item-service/src/main/java/hello/itemservice/domain/item/Item.java
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -15,9 +15,7 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
-    public Item(){
-
-    }
+    public Item(){}
 
     public Item(String itemName, Integer price, Integer quantity){
         this.itemName = itemName;
--- item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -1,9 +1,8 @@
-package hello.itemservice.domain.item.web.basic;
+package hello.itemservice.domain.web.basic;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +22,7 @@ public class BasicItemController {
     public String items(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items",items);
-        return "basic/item";
+        return "basic/items";
     }
 
     @PostConstruct
