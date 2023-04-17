8f09767985529a881baf12d41db6c6521daec99b
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-04-17 23:51:07 +0900

    스프링 MVC - 웹 페이지 만들기 (상품 상세)

--- item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -39,8 +40,9 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String
-
+    public String save(){
+        return "basic/addForm";
+    }
 
     @PostConstruct
     public void init(){
