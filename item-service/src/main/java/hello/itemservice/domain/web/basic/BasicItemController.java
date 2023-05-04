08f79e3283a4ce6a0223d06cc0532b458c5abce0
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-05-04 23:50:59 +0900

    스프링 MVC - 웹 페이지 만들기 (RedirectAttribute) 끝

--- item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -80,9 +81,17 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItem(Item item, Model model){
+    public String addItem5(Item item, Model model){
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String addItem6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
+        return "redirect:/basic/items/{itemId}";
     }
 
 
