dd096fb4bc1a3c0c74a633114c1694e0c826070c
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-06-19 23:53:13 +0900

    로그인 처리 - 쿠키를 사용한 구현

--- mvc2/login/src/main/java/hello/login/web/login/LoginController.java
+++ mvc2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -26,7 +29,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response){
         //검증 단계의 에러처리
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -40,6 +43,10 @@ public class LoginController {
         }
 
         //로그인 성공 처리
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(블라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId());
+        response.addCookie(idCookie);
+
         return "redirect:/";
     }
 }
