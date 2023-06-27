68a0f885dfd13f4c1b22f3b8ac231ec08af75362
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-06-27 22:53:44 +0900

    예외처리와 오류 페이지 끝

---
+++ mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,24 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        //RuntimeException 뿐만 아니라, 자식에 해당하는 모든 class들에게 해당된다.
+
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
+        //404가 발생하면, path에 해당하는 url을 호출해라.
+
+    }
+}
