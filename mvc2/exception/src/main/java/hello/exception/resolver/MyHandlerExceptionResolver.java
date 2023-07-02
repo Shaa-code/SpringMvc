bc17450cc7b04efd15985c089d35a5c991dcc30e
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-07-02 23:49:53 +0900

    API 예외처리 - HandlerExceptionResolver

--- mvc2/exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
+++ mvc2/exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -10,17 +10,21 @@ import java.io.IOException;
 
 @Slf4j
 public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
-
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
-        try{
-            if(ex instanceof IllegalArgumentException){
+        try {
+            //WAS에서 받아온 에러가 IllegalArgumentException이라면, response에 sendError를 넣어줌.
+            if (ex instanceof IllegalArgumentException) {
                 log.info("IllegalArgumentException resolver to 400");
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST,ex.getMessage());
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView();
             }
-        catch(IOException e){
-                ex.printStackTrace();
+        }catch(IOException e){
+                log.error("resolver ex", e);
         }
+
+        //return null로 두면,지정된 에러를 찾지 못했을 때,계속해서 에러가 WAS 쪽으로 나아간다.
+        return null;
     }
 }
