c167481e74045c78ed01cc5b0a1442393f6b4612
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-07-22 23:04:51 +0900

    Spring DB2 - MyBatis

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/MyBatisRepository.java
@@ -0,0 +1,39 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MyBatisRepository implements ItemRepository {
+//itemMapper를 implements 하는게 아니네? -> 빈으로 주입을 받으면
+    private final ItemMapper itemMapper;
+
+    @Override
+    public Item save(Item item) {
+        itemMapper.save(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        itemMapper.update(itemId, updateParam);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return itemMapper.findById(id);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        return itemMapper.findAll(cond);
+    }
+}
