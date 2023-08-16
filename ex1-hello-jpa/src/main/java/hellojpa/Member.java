5841b2d98ee372db0334e61f32320bb190a2dd00
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-08-16 23:41:33 +0900

    JPA - Embedded Type

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -14,11 +14,44 @@ public class Member{
     @Column(name = "USERNAME")
     private String username;
 
-
     //기간
+    @Embedded
     private Period workPeriod;
 
     //주소
+    @Embedded
     private Address homeAddress;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
 }
--- ex1-hello-jpa/src/main/java/hellojpa/Period.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Period.java
@@ -10,4 +10,19 @@ public class Period {
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
 }
