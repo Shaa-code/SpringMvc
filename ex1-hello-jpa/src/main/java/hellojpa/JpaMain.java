9f9369f7477b9225519f58d4fdf8f264037f31d8
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2023-08-26 23:11:28 +0900

    JPA - JPQL 기본문법

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,9 @@
 package hellojpa;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -13,19 +16,11 @@ public class JpaMain {
 
         try{
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street","10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new Address("old1", "street1", "10000"));
-            member.getAddressHistory().add(new Address("old2", "street2", "20000"));
-
-            em.persist(member);
-
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"),"kim"));
+            em.createQuery(cq).getResultList();
 
             tx.commit();
         }catch (Exception e){
